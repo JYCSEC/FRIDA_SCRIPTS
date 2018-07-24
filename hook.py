5ed4266b569b8f95b6f6936c7d7af0a9bdb7fc3c
JYCSEC <40046083+JYCSEC@users.noreply.github.com>
2018-07-24 10:44:31 -0400

    hooks to process app_to_hook

---
+++ hook.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import frida
+import sys
+
+session = frida.attach("app_to_hook")
+script = session.create_script("""
+Interceptor.attach(ptr("%s"), {
+    onEnter: function(args) {
+        send(args[0].toInt32());
+        }
+    });
+""" % int(sys.argv[1], 16))
+def on_message(message, data):
+    print(message)
+script.on('message', on_message)
+script.load()
+sys.stdin.read()
+
